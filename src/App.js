4c83c60abb15c1848daab808b1bf4de6ceb4f687
carlmenke <carlmenke13@gmail.com>
2025-07-21 18:01:15 -0700

    Refactor App component and styles for Tic Tac Toe game layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [rows, setRows] = useState([['none', 'none', 'none'],  ['none', 'none', 'none'], ['none', 'none', 'none']])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="rootContainer">
+      Tic Tac Toe
+      <div className='board'>
+
+        {rows.map((row) => {
+          return <div className='row'>
+           {row.map((cell) => {
+             return <div className='cell'>{cell}</div>
+            })}
+          </div>
+        })}
+
+      </div>
     </div>
   );
 }
